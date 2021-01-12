b65f16196681b791749b57103a39bb63b3d40ba7
Berkay Abay <berkayabay4@gmail.com>
2021-01-12 03:59:19 +0300

    12/01/21

--- Basic_conditionals.R
+++ Basic_conditionals.R
@@ -1,4 +1,4 @@
-# an example showing the general structure of an if-else statement
+# if-else yapýsý örneði
 a <- 0
 if(a!=0){
   print(1/a)
@@ -6,38 +6,43 @@ if(a!=0){
   print("No reciprocal for 0.")
 }
 
-# an example that tells us which states, if any, have a murder rate less than 0.5
+
+# murder rate deðeri 0.5 den küçük ise ekrana o deðeri yazdýrýr.
 library(dslabs)
 data(murders)
+
 murder_rate <- murders$total / murders$population*100000
+
 ind <- which.min(murder_rate)
+
 if(murder_rate[ind] < 0.5){
   print(murders$state[ind]) 
 } else{
   print("No state has murder rate that low")
 }
 
-# changing the condition to < 0.25 changes the result
+# murder rate deðeri 0.25 den küçük ise ekrana o deðeri yazdýrýr.
 if(murder_rate[ind] < 0.25){
   print(murders$state[ind]) 
 } else{
   print("No state has a murder rate that low.")
 }
 
-# the ifelse() function works similarly to an if-else conditional
+# ifelse() komuru ile ilk koþul saðlanýyorsa 2. deðeri, saðlanmýyor ise 3. deðeri döndürür.
 a <- 0
 ifelse(a > 0, 1/a, NA)
 
-# the ifelse() function is particularly useful on vectors
+# a>0 ise result deðerine 1/a yazdýr eðer deðil ise NA yaz
 a <- c(0,1,2,-4,5)
 result <- ifelse(a > 0, 1/a, NA)
 
-# the ifelse() function is also helpful for replacing missing values
+# ifelse() komutu ayrýca kayýp olan deðerlerler yerine baþka deðer atamak için kullanýlabilir.
 data(na_example)
 no_nas <- ifelse(is.na(na_example), 0, na_example) 
 sum(is.na(no_nas))
 
-# the any() and all() functions evaluate logical vectors
 z <- c(TRUE, TRUE, FALSE)
-any(z)
-all(z)
+
+any(z)    # any() komutunda herhangi bir deðer TRUE ise TRUE deðeri döndürür, deðil ise FALSE döndürür.
+
+all(z)    # all() komutunda bütün  deðerler TRUE ise TRUE deðeri döndürür, deðil ise FALSE döndürür.
